40d675e92c7ec046220f04ea37b166edae6b97ee
Kabita Joshi <kabitajoshi49@gmail.com>
2023-07-17 13:02:03 +0545

    it's responsive

--- index.js
+++ index.js
@@ -17,4 +17,10 @@ hamburger.addEventListener("click", () => {
   //   }
 });
 
+const schoolImages = [
+  " https://media.istockphoto.com/id/1363289778/photo/british-secondary-school.jpg?s=1024x1024&w=is&k=20&c=mBxiguG3CdnbWAvrdnMxE6ndH2M7zZxcXOR57IVQMZw=",
+  "https://images.unsplash.com/photo-1541339907198-e08756dedf3f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+  "https://images.unsplash.com/photo-1498243691581-b145c3f54a5a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+];
 
+let index=0;
